2daabc75b7b34c630336156426333971c7dff78b
denverschleifer <dschleifer@student.cvtc.edu>
2017-09-18 17:27:20 -0500

    initial commit

---
+++ test/PaintCalculatorTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author Denver
+ *
+ */
+public class PaintCalculatorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
